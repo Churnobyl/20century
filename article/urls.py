dd448d6ed2224575a7f26ef5926276e2e9d54172
louis N <louis_n@naver.com>
2023-05-09 11:41:31 +0900

    article CRU

--- article/urls.py
+++ article/urls.py
@@ -3,5 +3,10 @@ from article import views
 
 
 urlpatterns = [
-
+    path("", views.ArticleView.as_view(), name="article_view"),
+    path(
+        "<int:article_id>/",
+        views.ArticleDetailView.as_view(),
+        name="article_detail_view",
+    ),
 ]
