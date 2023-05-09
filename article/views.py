f208d0b3fa6d9c2373888c35c9b6d6b58ba1720c
geunhye-kim <mykgh6514@naver.com>
2023-05-09 15:18:42 +0900

    :sparkles: Created article comment
    article comment!!!!!

--- article/views.py
+++ article/views.py
@@ -2,12 +2,13 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework import status, permissions
 from rest_framework.response import Response
-from article.models import Article
+from article.models import Article, Comment
 from article.serializers import (
     ArticleSerializer,
     ArticleListSerializer,
     ArticleCreateSerializer,
     ArticleUpdateSerializer,
+    CommentSerializer,
 )
 from article.serializers import BookmarkSerializer
 
@@ -48,7 +49,6 @@ class ArticleDetailView(APIView):
         else:
             return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
         
-
 class BookmarkView(APIView):
     def get(self, request, article_id):
         article = get_object_or_404(Article, id=article_id)
@@ -63,3 +63,44 @@ class BookmarkView(APIView):
         else:
             article.bookmarked.add(request.user)
             return Response({"message": "북마크에 추가했습니다."}, status=status.HTTP_200_OK)
+        
+
+class CommentView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    # 댓글 작성
+    def post(self, request, article_id):
+        serializer = CommentSerializer(data=request.data)
+        article = Article.objects.get(id=article_id)
+        if serializer.is_valid():
+            serializer.save(user=request.user, article=article)
+            return Response({'message':'댓글 작성 완료'}, status=status.HTTP_201_CREATED)
+        else:    
+            return Response({'error':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetailView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    # 댓글 수정
+    def put(self, request, article_id, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id, article_id=article_id)
+        article = Article.objects.get(id=article_id)
+        if request.user == comment.user:
+            serializer = CommentSerializer(comment, data=request.data)
+            if serializer.is_valid():
+                serializer.save(user=request.user, article=article)
+                return Response({'message':'댓글 수정 완료'}, status=status.HTTP_200_OK)
+            else:    
+                return Response({'error':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error':'권한이 없습니다!'}, status=status.HTTP_401_UNAUTHORIZED)
+
+    # 댓글 삭제
+    def delete(self, request, article_id, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        if request.user == comment.user:
+            comment.delete()
+            return Response({'message':'댓글 삭제 완료'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error':'권한이 없습니다!'}, status=status.HTTP_401_UNAUTHORIZED)
