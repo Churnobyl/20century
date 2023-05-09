4cd088dfee170ee04a0deb48f13ea6e750ed2182
Churnobyl <tjdcjfals@gmail.com>
2023-05-09 17:17:59 +0900

    :sparkles: Add user profile image
    유저 프로필 이미지 추가

--- centurymuseum/settings.py
+++ centurymuseum/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
