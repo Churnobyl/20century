337f9bf1172081c33238a22e184fa7b0ea87fecc
louis N <louis_n@naver.com>
2023-05-11 21:25:01 +0900

    임시 저장

--- article/apps.py
+++ article/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from centurymuseum import settings
 
 
 class ArticleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'article'
+
+    # def ready(self):
+    #     if settings.SCHEDULER_DEFAULT:
+    #         from . import scheduler
+    #         scheduler.start()
